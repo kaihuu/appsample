9741a835f003026370ec870e177fc3ac8a1af2cd
kaihuu <uemura-toshiaki-xh@ynu.jp>
2017-11-12 20:38:18 +0900

    modify

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'main_menu/index'
+
   resources :references
   resources :types
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
